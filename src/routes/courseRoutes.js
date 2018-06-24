3d95b3ea5c9a6575638ead369d2e2ababc0d5619
kai2june <104703027@nccu.edu.tw>
2018-06-24 15:48:14 +0800

    all selected class on the curriculum still a little proble of <td>

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -52,61 +52,13 @@ const router = function () {
                     }
                     let rlt_findManyCourses = [];
                     rlt_findManyCourses = await coll.find({ $or: query_arr }).toArray();
-                    
-                    // const color = {
-                    //     literature: 'black',
-                    //     science:'brown',
-                    //     society:'red',
-                    //     law:'orange',
-                    //     commerce:'yellow',
-                    //     foreign:'green',
-                    //     broadcast:'blue',
-                    //     international:'violet',
-                    //     education:'grey'
-                    // };
-                    
-                    // let academy = req.body.academy;
-                    // for(let i =0; i<academy.length; i++){
-                    //     switch(academy[i]){
-                    //     case 'literature':
-                    //         academy[i].color = 'black';
-                    //         break;
-                    //     case 'science':
-                    //         academy[i].color = 'brown';
-                    //         break;
-                    //     case 'society':
-                    //         academy[i].color = 'red';
-                    //         break;
-                    //     case 'law':
-                    //         academy[i].color = 'orange';
-                    //         break;
-                    //     case 'commerce':
-                    //         academy[i].color = 'yellow';
-                    //         break;
-                    //     case 'foreign':
-                    //         academy[i].color = 'green';
-                    //         break;
-                    //     case 'broadcast':
-                    //         academy[i].color = 'blue';
-                    //         break;
-                    //     case 'international':
-                    //         academy[i].color = 'violet';
-                    //         break;
-                    //     case 'education':
-                    //         academy[i].color = 'grey';
-                    //         break;
-                    //     }
-                    //     console.log(academy[i].color); 
-                    // }
 
                     res.render('course', {
-                        // color: color,
                         academy: req.body.academy,
                         degree: req.body.degree,
                         department: req.body.department,
                         course: rlt_findManyCourses
-                    }
-                    );
+                    });
                 }catch(err){
                     if(err)
                         console.log(err);
@@ -118,10 +70,3 @@ const router = function () {
 };
 
 module.exports = router;
-// <%if(i==0){%>
-//     <td style="background-color: yellow;"></td>
-// <%}else if(i==1){%>
-//     <td style="background-color: grey;"></td>
-// <%}else if(i==2){%>
-//     <td style="background-color: violet;"></td>
-// <%}%>
--- src/routes/curriculumRoutes.js
+++ src/routes/curriculumRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+const {MongoClient} = require('mongodb');
 
 const curriculumRouter = express.Router();
+
+const weekday = ['星期一','星期二','星期三','星期四','星期五'];
+
 const router = function () {
     curriculumRouter.use((req, res, next) => {
         if (req.user) {
@@ -12,10 +15,38 @@ const router = function () {
     });
     curriculumRouter.route('/')
         .get((req, res) => {
-            res.render('curriculum');
+
+            res.send('This is curriculum GET');
+            // res.render('curriculum', {
+            //     weekday: weekday
+            // });
         })
         .post( (req,res) => {
-            res.send(req.body);
+            // res.send(req.body.class);
+            (async function findSpecifiedCourses(){
+                const url = 'mongodb://localhost:27017';
+                const dbName = 'courseApp';
+                try{
+                    const client = await MongoClient.connect(url);
+                    const db = client.db(dbName);
+                    const coll = db.collection('courses');
+                    let query_arr = [];
+                    for(let i=0; i< req.body.class.length; i++){
+                        let query = {
+                            class: req.body.class[i]
+                        };
+                        query_arr[i] = query;
+                    }
+                    const rlt_findSpecifiedCourses = await coll.find({ $or: query_arr }).toArray();
+                    res.render('curriculum', {
+                        weekday: weekday,
+                        course: rlt_findSpecifiedCourses    
+                    })
+                }catch(err){
+                    if(err)
+                        console.log(err);
+                }
+            }());
         })
 
     return curriculumRouter;
